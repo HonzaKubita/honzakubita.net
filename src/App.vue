aa0232dad75424038681f8482d241b1df0571247
Jan Kubita <honza.kubita@gmail.com>
2022-06-22 22:15:23 +0200

    buy me a coffe

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
 
 <script>
 console.log("%cYou're not welcome here", 'font-size: 40px; color: red; font-weight: bold;');
+
+
+export default {
+  name: "App",
+}
+
 </script>
 
 <style>
@@ -11,17 +17,6 @@ body {
   background: #1b1b1b;
   height: 100%;
   overflow: hidden;
-}
-@keyframes gradient {
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-	}
 }
 img {
   user-drag: none;
